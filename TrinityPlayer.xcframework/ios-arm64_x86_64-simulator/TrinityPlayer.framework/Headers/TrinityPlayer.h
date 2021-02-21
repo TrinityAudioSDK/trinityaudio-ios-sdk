97f8284c27ddb1342c71e756b2121467b14c36e6
mgmg-jenkins <mgmg-jenkins@memeglobal.tech>
2021-02-21 09:10:29 -0600

    Version 0.1.9 published

---
+++ TrinityPlayer.xcframework/ios-arm64_x86_64-simulator/TrinityPlayer.framework/Headers/TrinityPlayer.h
@@ -0,0 +1,18 @@
+//
+//  TrinityPlayer.h
+//  TrinityPlayer
+//
+//  Created by Kuznetsov Roman on 1/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrinityPlayer.
+FOUNDATION_EXPORT double TrinityPlayerVersionNumber;
+
+//! Project version string for TrinityPlayer.
+FOUNDATION_EXPORT const unsigned char TrinityPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrinityPlayer/PublicHeader.h>
+
+
